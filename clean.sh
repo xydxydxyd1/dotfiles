3a8e3245cb8c969512135e8324ccd01cd65c3176
xydxydxyd1 <xydxydxyd1@gmail.com>
2023-06-04 16:38:22 -0700

    remove vim plugins with clean

--- clean.sh
+++ clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# restore to pre-setup state
 rm -rf "$HOME/.zshrc"
 rm -rf "$HOME/.profile"
+
+rm -rf "$HOME/.vim/plugged"
 rm -rf "$HOME/.vim"
+
 rm -rf "$HOME/.config/nvim"
 rm -rf "$HOME/.config/gdb"
