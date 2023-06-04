3cf1efb3d15e754d7564d80bc721aa10c71ae408
xydxydxyd1 <xydxydxyd1@gmail.com>
2023-06-04 16:42:31 -0700

    install vim plugin with setup

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -s "$HOME/.dotfiles/bash/bashrc" "$HOME/.zshrc"
 ln -s "$HOME/.dotfiles/bash/bashrc" "$HOME/.profile"
 # vim
 ln -s "$HOME/.dotfiles/vim" "$HOME/.vim"
+vim -c PlugInstall -c qa
 
 # Link to .config directory
 # nvim
