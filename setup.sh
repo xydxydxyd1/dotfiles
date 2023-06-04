613bb1ad601540b0cbb6f32993afde675b5c8e8d
xydxydxyd1 <xydxydxyd1@gmail.com>
2023-06-04 16:31:44 -0700

    add quotes in scripts

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 
 # Link to home directory
 # bash
-ln -s $HOME/.dotfiles/bash/bashrc $HOME/.zshrc
+ln -s "$HOME/.dotfiles/bash/bashrc" "$HOME/.zshrc"
 # zsh
-ln -s $HOME/.dotfiles/bash/bashrc $HOME/.profile
+ln -s "$HOME/.dotfiles/bash/bashrc" "$HOME/.profile"
 # vim
-ln -s $HOME/.dotfiles/vim $HOME/.vim
+ln -s "$HOME/.dotfiles/vim" "$HOME/.vim"
 
 # Link to .config directory
 # nvim
-ln -s $HOME/.dotfiles/vim $HOME/.config/nvim
+ln -s "$HOME/.dotfiles/vim" "$HOME/.config/nvim"
 # gdb
-ln -s $HOME/.dotfiles/gdb $HOME/.config/gdb
+ln -s "$HOME/.dotfiles/gdb" "$HOME/.config/gdb"
 
 read -r -p "CXX Compiler: " cxx
-echo "export CXX='$cxx'" >> $HOME/.dotfiles/bash/extraconf.sh
+echo "export CXX='$cxx'" >> "$HOME/.dotfiles/bash/extraconf.sh"
