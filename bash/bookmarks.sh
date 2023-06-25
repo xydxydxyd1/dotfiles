6770d87ed65cd8949632a4a2919c56878221ff70
Eric Xu <xydxydxyd1@gmail.com>
2023-06-25 16:01:12 -0700

    Fix bug: quote surrounding directory name escaped improperly
    set_mark new feature: if mark already exists, prompt for override

--- bash/bookmarks.sh
+++ bash/bookmarks.sh
@@ -9,13 +9,25 @@ fi
 
 
 set_mark(){
-	# TODO: Input Validation <26-01-23, xydxydxyd1> #
 	# TODO: Verbosity level <26-01-23, xydxydxyd1> #
 	# TODO: Bookmark Description <26-01-23, xydxydxyd1> #
-	pwd | xargs printf "mk_$1=\'%s\'\n"
-	pwd | xargs printf "mk_$1=\'%s\'\n" >> "$bookmarks"
+
+	mark_name="mk_$1"
+
+	if [[ -n ${!mark_name} ]]; then
+		read -r -p 'Mark already exists. Override (y/n)? ' yn
+		if [[ "$yn" != 'y' ]]; then
+			exit 1
+		fi
+
+		# remove previous mark_name entry
+		grep -q -v "$mark_name" < $bookmarks > $bookmarks
+	fi
+
+	pwd | xargs printf "$mark_name='%s'\n"
+	pwd | xargs printf "$mark_name='%s'\n" >> "$bookmarks"
+
 	source "$bookmarks"	# reload bookmarks
 }
-
 
 # TODO: rm_mark <26-01-23, xydxydxyd1> #
