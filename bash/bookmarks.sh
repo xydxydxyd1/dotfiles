2540fc5c0d7ec1d27cb9b38c38bdc01bacf7e9be
Eric Xu <xydxydxyd1@gmail.com>
2023-08-16 13:51:50 -0700

    Remake bookmark system: Now bookmarks are stored as symlinks in $mk; `cd` now automatically lists newly entered directory

--- bash/bookmarks.sh
+++ bash/bookmarks.sh
@@ -1,46 +1,48 @@
-mydir="$( dirname -- "${BASH_SOURCE[0]}" )"
-bookmarks="$mydir/bookmarks.txt"
+#!/bin/bash
 
-# Import all bookmarks
+# Set default bookmark directory
+[ -n "$mark_folder" ] || mark_folder="$HOME/.mk"
 
-if [[ -f "$bookmarks" ]]; then
-    source "$bookmarks"
-fi
+# Create bookmark directory if not present
+[ -d "$mark_folder" ] || mkdir "$mark_folder"
 
+# For easy access of bookmarks: $mk/mark_name
+export mk="$mark_folder"
 
 set_mark(){
 	# TODO: Verbosity level <26-01-23, xydxydxyd1> #
 	# TODO: Bookmark Description <26-01-23, xydxydxyd1> #
 
-	mark_name="mk_$1"
+	curr_mark_path="$mark_folder/$1"
 
-	if [[ -n ${!mark_name} ]]; then
+	if [[ -e $curr_mark_path ]]; then
 		read -r -p 'Mark already exists. Override (y/n)? ' yn
-		if [[ "$yn" != 'y' ]]; then
-			return
-		fi
+        if [[ "$yn" != 'y' ]]; then
+            echo 'Aborting...'
+            return
+        fi
 
-		# remove previous mark_name entry
-		sed -i -e "/$mark_name/d" "$bookmarks"
+		# remove previous curr_mark_path entry
+        rm -f "$curr_mark_path"
 	fi
 
-	pwd | xargs printf "$mark_name='%s'\n"
-	pwd | xargs printf "$mark_name='%s'\n" >> "$bookmarks"
-
-	source "$bookmarks"	# reload bookmarks
+    # Create symlink
+    ln -s "$(pwd)" "$curr_mark_path"
+    echo "Symlink created: $curr_mark_path --> $(pwd)"
 }
 
 rm_mark(){
-	mark_name="mk_$1"
-	if [[ -z $(cat $bookmarks | grep -e $mark_name=) ]]; then
-		echo "$mark_name does not exist"
-		return
-	fi
-	read -r -p "Remove $mark_name?" yn
+	curr_mark_path="$mark_folder/$1"
+
+    if [[ ! -e "$curr_mark_path" ]]; then
+        echo "$curr_mark_path does not exist"
+        return
+    fi
+
+	read -r -p "Remove $curr_mark_path?" yn
 	if [[ "$yn" != 'y' ]]; then
-		echo "Aborted"
+		echo "Aborting..."
 		return
 	fi
-	sed -i -e "/$mark_name/d" "$bookmarks"
-	echo "restart shell for removal to take effect"
+    rm -f "$curr_mark_path"
 }
