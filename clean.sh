91a21ddb1e4aca18f1304be8b89ec479f8db3aa4
Eric Xu <xydxydxyd1@gmail.com>
2024-08-01 15:03:51 -0700

    Add option to delete script

--- clean.sh
+++ clean.sh
@@ -3,7 +3,11 @@
 
 rm -rf "$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.profile"
 rm -rf "$HOME/.editorconfig"
-rm -rf "$HOME/scripts"
+
+read -r -p 'Delete scripts? This includes extra scripts that may not be saved ' yn
+if [[ "$yn" == 'y' ]]; then
+    rm -rf "$HOME/scripts"
+fi
 
 rm -rf "$HOME/.vim/plugged"
 rm -rf "$HOME/.vim"
