0a33d0a4b817ded00464765b0bcb582bdcdc4094
Eric Xu <xydxydxyd1@gmail.com>
2024-09-13 16:38:38 -0700

    Move snippets to completion.lua

---
+++ vim/lua/plugins/completion.lua
@@ -0,0 +1,4 @@
+return {
+    {"SirVer/ultisnips"},
+    {"honza/vim-snippets"},
+}
--- vim/lua/plugins/misc.lua
+++ vim/lua/plugins/misc.lua
@@ -1,8 +1,6 @@
 return {
-    {"SirVer/ultisnips"},
     {"tpope/vim-fugitive"},
     {"junegunn/fzf"},
     {"junegunn/fzf.vim"},
-    {"honza/vim-snippets"},
     {"neovim/nvim-lspconfig"},
 }
