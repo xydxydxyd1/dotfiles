59aee36cf6085ff46272671b8995cfacd34d7167
Eric Xu <xydxydxyd1@gmail.com>
2024-09-13 16:54:17 -0700

    Add <C-j> and <C-k> for completion selection

--- vim/lua/plugins/completion.lua
+++ vim/lua/plugins/completion.lua
@@ -8,13 +8,12 @@ function cmp_config()
                 vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
             end,
         },
-        mapping = cmp.mapping.preset.insert({
+        mapping = {
+            ['<C-j>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+            ['<C-k>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
+        },
         sources = cmp.config.sources({
             { name = 'nvim_lsp' },
             { name = 'ultisnips' }, -- For ultisnips users.
