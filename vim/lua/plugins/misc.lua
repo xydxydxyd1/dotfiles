e9ab08fb2481a590a1a6f029d890811edf63360a
Eric Xu <xydxydxyd1@gmail.com>
2024-09-13 17:03:01 -0700

    Add keymaps for FZF.vim

--- vim/lua/plugins/misc.lua
+++ vim/lua/plugins/misc.lua
@@ -1,6 +1,12 @@
 return {
     {"tpope/vim-fugitive"},
     {"junegunn/fzf"},
-    {"junegunn/fzf.vim"},
+    {
+        "junegunn/fzf.vim",
+        init = function()
+            vim.api.nvim_set_keymap("n", "<C-n>", ":GFiles<CR>", {noremap = true})
+            vim.api.nvim_set_keymap("n", "<C-m>", ":Buffer<CR>", {noremap = true})
+        end
+    },
     {"neovim/nvim-lspconfig"},
 }
