84d8ab1a88650c21fc8f9da3d4e78066d1a830d6
Eric Xu <xydxydxyd1@gmail.com>
2024-09-15 18:30:21 -0700

    Add vimtex

--- vim/lua/plugins/misc.lua
+++ vim/lua/plugins/misc.lua
@@ -32,4 +32,12 @@ return {
             })
         end
     },
+    {
+        "lervag/vimtex",
+        lazy = false,
+        init = function()
+            vim.g.vimtex_view_general_viewer = "okular"
+            vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+        end
+    },
 }
