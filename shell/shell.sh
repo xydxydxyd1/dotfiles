1e890c9415381e57a582a68f3cff70a60df9f7b7
Eric Xu <xydxydxyd1@gmail.com>
2025-01-25 23:00:40 -0800

    fix: bookmark env

--- shell/shell.sh
+++ shell/shell.sh
@@ -8,6 +8,10 @@ export CLICOLOR=1
 set -o vi	# vi keybinds to shell
 export KEYTIMEOUT=1 # Reduce key timeout for vi mode
 
+# Bookmarks
+export Mk="$HOME/.mk"
+export mk="$HOME/.mk"
+
 # Third party tools
 echo "Setting up NVM..."    # Usually the most time-consuming step
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
