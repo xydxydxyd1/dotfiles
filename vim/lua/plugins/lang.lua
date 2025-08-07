9ecb91305bad0b55780c356de85b9ce04449031b
Eric Xu <xydxydxyd1@gmail.com>
2025-08-07 14:02:58 -0700

    fix rust analyzer wrong key

--- vim/lua/plugins/lang.lua
+++ vim/lua/plugins/lang.lua
@@ -2,7 +2,29 @@
 
 -- Keys are langauge servers accepted by lspconfig. Values are passed to setup
 lsp_setups = {
-    rust = {
+    ["rust_analyzer"] = {
+        cmd = { "rust-analyzer" },
+        filetypes = { "rust" },
+        root_dir = function(fname)
+              local util = require("lspconfig.util")
+              local cargo_crate_dir = util.root_pattern 'Cargo.toml'(fname)
+              local cmd = 'cargo metadata --no-deps --format-version 1'
+              if cargo_crate_dir ~= nil then
+                cmd = cmd .. ' --manifest-path ' .. util.path.join(cargo_crate_dir, 'Cargo.toml')
+              end
+              local cargo_metadata = vim.fn.system(cmd)
+              local cargo_workspace_dir = nil
+              if vim.v.shell_error == 0 then
+                cargo_workspace_dir = vim.fn.json_decode(cargo_metadata)['workspace_root']
+              end
+              return cargo_crate_dir
+                or util.root_pattern 'rust-project.json'(fname)
+                or util.find_git_ancestor(fname)
+              -- return cargo_workspace_dir
+              --   or cargo_crate_dir
+              --   or util.root_pattern 'rust-project.json'(fname)
+              --   or util.find_git_ancestor(fname)
+            end,
         settings = {
             ['rust-analyzer'] = {
                 diagnostics = {
@@ -22,6 +44,7 @@ return {
         init = function()
             installed_lsps = require("config.local").installed_lsps
             for _, lsp in ipairs(installed_lsps) do
+                --vim.api.nvim_echo({{"Loading " .. lsp, "Normal"}}, false, {})
                 args = lsp_setups[lsp]
                 if args == nil then args = {} end
                 require("lspconfig")[lsp].setup(args)
